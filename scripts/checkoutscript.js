15fd8c5a3a41643da1d47c18d1c96e35a8354c74
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2022-12-15 14:57:03 +0100

    Adding all the const

--- scripts/checkoutscript.js
+++ scripts/checkoutscript.js
@@ -3,7 +3,15 @@ const lastnameInput = document.querySelector("#lastname")
 const addressInput = document.querySelector("#address-field")
 const zipCodeInput = document.querySelector("#zipcode")
 const countryInput = document.querySelector("#country")
-const phoneNumberInput = document.querySelector("checkout-phone")
-const emailInput = document.querySelector("checkout-email")
+const phoneNumberInput = document.querySelector("#checkout-phone")
+const emailInput = document.querySelector("#checkout-email")
+
+const emailError = document.querySelector("#email-checkout-error")
+const phoneError = document.querySelector("#phone-checkout-error")
+const countryError = document.querySelector("#country-checkout-error")
+const zipCodeError = document.querySelector("#zipcode-checkout-error")
+const addressError = document.querySelector("#address-checkout-error")
+const lastnameError = document.querySelector("#lastname-checkout-error")
+const firstnameError = document.querySelector("#firstname-checkout-error")
 
 
