418ea3a1fb7a574dc253cde52c71358a3ad8d892
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2022-12-15 14:19:07 +0100

    Update checkout html and script

---
+++ scripts/checkoutscript.js
@@ -0,0 +1,9 @@
+const firstnameInput = document.querySelector("#firstname")
+const lastnameInput = document.querySelector("#lastname")
+const addressInput = document.querySelector("#address-field")
+const zipCodeInput = document.querySelector("#zipcode")
+const countryInput = document.querySelector("#country")
+const phoneNumberInput = document.querySelector("checkout-phone")
+const emailInput = document.querySelector("checkout-email")
+
+
