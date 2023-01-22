b0e9689f4d259db8838fff3017105580283ee1dc
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2023-01-22 19:51:52 +0100

    add featured.js

--- scripts/featured.js
+++ scripts/featured.js
@@ -1,3 +1,29 @@
 const url = "https://sanna.codes/wp-json/wc/store/products";
 
+const popularGames = document.querySelector("#popular-container");
+const featuredGames = document.querySelector("#featured-container");
 
+let list = [];
+
+const getFeatured = async() => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++){
+            const tagData = data[i]["tags"][0]["name"];
+        
+
+            if (tagData === "featured"){
+
+            }
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+getFeatured();
